08b9164e436aaa0d33be8989d27b391c9bd3ca5d
Yasin Ertekin <yasinertekin24@gmail.com>
2024-02-26 16:51:05 +0300

    Add AuthInterface and AuthImpl classes

---
+++ lib/product/service/auth_interface.dart
@@ -0,0 +1,50 @@
+import 'package:bloc_example/product/model/users.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+/// AuthInterFace
+abstract class AuthInterFace {
+  /// signWithEmailAndPassword
+  Future<UserCredential> signWithEmailAndPassword(
+    Users users,
+  );
+
+  /// signUpWithEmailAndPassword
+  Future<UserCredential> signUpWithEmailAndPassword(
+    Users users,
+  );
+}
+
+/// AuthImpl
+final class AuthImpl implements AuthInterFace {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Future<UserCredential> signWithEmailAndPassword(Users users) async {
+    try {
+      final userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: users.email,
+        password: users.password,
+      );
+      return userCredential;
+    } catch (e) {
+      // Hata oluştuğunda yapılacak işlemler
+      print('Hata: $e');
+      rethrow; // Hatanın yukarıya fırlatılması (yalnızca gerekirse)
+    }
+  }
+
+  @override
+  Future<UserCredential> signUpWithEmailAndPassword(Users users) async {
+    try {
+      final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: users.email,
+        password: users.password,
+      );
+      return userCredential;
+    } catch (e) {
+      // Hata oluştuğunda yapılacak işlemler
+      print('Hata: $e');
+      rethrow; // Hatanın yukarıya fırlatılması (yalnızca gerekirse)
+    }
+  }
+}
