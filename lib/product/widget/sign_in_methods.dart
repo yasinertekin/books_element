8c9ffaac9f012d8af043e78199ac2228e5fc2255
Yasin Ertekin <yasinertekin24@gmail.com>
2024-02-27 22:13:05 +0300

    Add SignInMethods widget

---
+++ lib/product/widget/sign_in_methods.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+/// Sign In Methods
+final class SignInMethods extends StatelessWidget {
+  /// Sign In Methods Constructor
+  const SignInMethods({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        OutlinedButton(
+          onPressed: () {},
+          child: const Text('Google'),
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: const Text('Facebook'),
+        ),
+      ],
+    );
+  }
+}
