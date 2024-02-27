d7880f5390c6dc5e98cd514ca39ba924a848bc1d
Yasin Ertekin <yasinertekin24@gmail.com>
2024-02-27 22:12:42 +0300

    Add navigation mixin for easy navigation

---
+++ lib/product/mixin/navigation_mixin.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+/// Navigation Mixin
+mixin NavigationMixin {
+  /// Navigate to Back
+  void navigateToBack(BuildContext context) {
+    context.router.pop();
+  }
+
+  /// Navigate to Custom Route
+  void navigateToNamedRoute(BuildContext context, String route) {
+    context.router.pushNamed(route);
+  }
+}
