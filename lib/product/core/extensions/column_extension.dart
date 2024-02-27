502777885c70a61309979363275c532f068a5fc0
Yasin Ertekin <yasinertekin24@gmail.com>
2024-02-27 22:12:02 +0300

    Add spaceBetweenWidgets extension to Column

---
+++ lib/product/core/extensions/column_extension.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+/// Extension for space between widgets
+extension SpaceBetweenWidgets on Column {
+  /// Space between widgets
+  Column space(double spaceAmount) {
+    return Column(
+      mainAxisAlignment: mainAxisAlignment,
+      mainAxisSize: mainAxisSize,
+      crossAxisAlignment: crossAxisAlignment,
+      verticalDirection: verticalDirection,
+      textDirection: textDirection,
+      textBaseline: textBaseline,
+      children: List<Widget>.generate(
+        children.length * 2 - 1,
+        (index) {
+          if (index.isEven) {
+            return children[index ~/ 2];
+          } else {
+            return SizedBox(height: spaceAmount);
+          }
+        },
+      ),
+    );
+  }
+}
