21a2378976d6572b7dc65911ef892b00796fdfb0
Yasin Ertekin <yasinertekin24@gmail.com>
2024-02-27 22:11:56 +0300

    Add navigation constants

---
+++ lib/product/core/constants/navigation_constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+@immutable
+
+/// Navigation Constants
+final class NavigationConstants {
+  const NavigationConstants._();
+
+  /// Login View
+  static const String loginView = '/login';
+
+  /// Register View
+  static const String registerView = '/register';
+
+  /// Home View
+  static const String homeView = '/home';
+}
