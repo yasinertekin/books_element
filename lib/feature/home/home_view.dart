97a543a36e0f12073049eeb792531821e78c36cd
Yasin Ertekin <yasinertekin24@gmail.com>
2024-02-27 22:11:51 +0300

    Remove unused import and color reference in HomeView

--- lib/feature/home/home_view.dart
+++ lib/feature/home/home_view.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:bloc_example/gen/colors.gen.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
+
+/// Home View
 final class HomeView extends StatelessWidget {
+  /// Home View Constructor
   const HomeView({super.key});
 
   @override
@@ -14,7 +16,6 @@ final class HomeView extends StatelessWidget {
           'Home',
           style: TextStyle(
             fontSize: 30,
-            color: ColorName.lightThemeColor,
           ),
         ),
       ),
