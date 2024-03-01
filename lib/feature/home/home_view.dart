5f081e63f229ff4c08d503ad516a83c3a88d4380
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-01 16:57:16 +0300

    Add search functionality to HomeView

--- lib/feature/home/home_view.dart
+++ lib/feature/home/home_view.dart
@@ -3,14 +3,20 @@ import 'package:bloc_example/feature/index.dart';
 @RoutePage()
 
 /// Home View
-final class HomeView extends StatelessWidget {
+final class HomeView extends StatelessWidget with NavigationMixin {
   /// Home View Constructor
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          navigateToNamedRoute(context, NavigationEnum.searchView.value);
+        },
+        child: const Icon(Icons.search),
+      ),
+      body: const Center(
         child: Text(
           'Home',
           style: TextStyle(
