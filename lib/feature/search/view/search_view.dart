61ecf579990b5c68bc359e78db98ce5dbf8b9158
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-02 23:28:03 +0300

    Add custom network image widget and update search view layout

--- lib/feature/search/view/search_view.dart
+++ lib/feature/search/view/search_view.dart
@@ -2,6 +2,7 @@ import 'package:bloc_example/feature/index.dart';
 import 'package:bloc_example/feature/search/view_model/cubit/books_cubit.dart';
 import 'package:bloc_example/feature/search/view_model/cubit/state/books_state.dart';
 import 'package:bloc_example/product/model/book/books/books.dart';
+import 'package:bloc_example/product/widget/custom_network_image.dart';
 
 @RoutePage()
 final class SearchView extends StatelessWidget {
@@ -9,31 +10,94 @@ final class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = TextEditingController();
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<BooksCubit>().getBooks('Flutter');
-        },
-        child: const Icon(Icons.search),
+      appBar: AppBar(
+        title: const Text('Search'),
       ),
-      body: BlocBuilder<BooksCubit, BooksState>(
-        builder: (context, state) {
-          if (state.isLoding) {
-            return const CustomLoading();
-          } else if (state.error != null) {
-            return _ErrorText(
-              error: state.error!,
-            );
-          } else if (state.books != null) {
-            return _BookList(
-              books: state.books!,
-            );
-          } else {
-            return const Center(
-              child: Text('Enter a search term to begin'),
-            );
-          }
-        },
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: context.dynamicWidth(0.45),
+              height: context.dynamicHeight(0.5),
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: context.height / 11,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      width: context.dynamicWidth(0.45),
+                      height: context.dynamicHeight(0.2),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFF2E9D3),
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: context.height / 8.5,
+                    left: 0,
+                    right: 0,
+                    child: const CustomNetworkImage(
+                      height: 0.3,
+                      fit: BoxFit.contain,
+                      imageUrl:
+                          'https://assets.api.uizard.io/api/cdn/stream/6dbd88cc-4f52-4da3-baae-b339dfd7e52c.png',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            CustomTextField(
+              onFieldSubmitted: (value) {
+                context.read<BooksCubit>().getBooks(value);
+              },
+              textInputAction: TextInputAction.search,
+              controller: controller,
+              labelText: 'Search',
+            ),
+            const _BookListBlocBuilder(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final class _BookListBlocBuilder extends StatelessWidget {
+  const _BookListBlocBuilder({
+    this.height = 0.5,
+  });
+
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        height: context.dynamicHeight(height),
+        child: BlocBuilder<BooksCubit, BooksState>(
+          builder: (context, state) {
+            if (state.isLoding) {
+              return const CustomLoading();
+            } else if (state.error != null) {
+              return _ErrorText(
+                error: state.error!,
+              );
+            } else if (state.books != null) {
+              return _BookList(
+                books: state.books!,
+              );
+            } else {
+              return const Center(
+                child: Text('Enter a search term to begin'),
+              );
+            }
+          },
+        ),
       ),
     );
   }
@@ -42,7 +106,6 @@ final class SearchView extends StatelessWidget {
 final class _BookList extends StatelessWidget {
   const _BookList({
     required this.books,
-    super.key,
   });
 
   final Books books;
@@ -50,12 +113,18 @@ final class _BookList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: books.items!.length,
+      scrollDirection: Axis.horizontal,
+      itemCount: books.items?.length,
       itemBuilder: (context, index) {
-        final item = books.items![index];
-        return ListTile(
-          title: Text(item.volumeInfo!.title!),
-          subtitle: Text(item.volumeInfo!.authors!.join(', ')),
+        final item = books.items?[index];
+        return Stack(
+          children: [
+            Center(
+              child: CustomNetworkImage(
+                imageUrl: item?.volumeInfo?.imageLinks?.thumbnail,
+              ),
+            ),
+          ],
         );
       },
     );
@@ -65,7 +134,6 @@ final class _BookList extends StatelessWidget {
 final class _ErrorText extends StatelessWidget {
   const _ErrorText({
     required this.error,
-    super.key,
   });
 
   final String error;
@@ -74,6 +142,24 @@ final class _ErrorText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text(error),
+    );
+  }
+}
+
+class ImageWidget extends StatelessWidget {
+  const ImageWidget({
+    super.key,
+    this.imageUrl =
+        'https://assets.api.uizard.io/api/cdn/stream/6dbd88cc-4f52-4da3-baae-b339dfd7e52c.png',
+  });
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      width: context.dynamicWidth(0.4),
+      height: context.dynamicHeight(0.25),
     );
   }
 }
