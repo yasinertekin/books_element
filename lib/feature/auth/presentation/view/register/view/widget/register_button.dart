db8dae3c37c86dfad2edf65104bad3dfe66f9ef6
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-14 16:52:56 +0300

    Add RegisterButton widget to register view

---
+++ lib/feature/auth/presentation/view/register/view/widget/register_button.dart
@@ -0,0 +1,46 @@
+part of '../register_view.dart';
+
+final class _RegisterButton extends StatelessWidget {
+  const _RegisterButton({
+    required this.formKey,
+    required this.registerCubit,
+    required this.emailController,
+    required this.passwordController,
+    required this.repeatPasswordController,
+  });
+
+  final GlobalKey<FormState> formKey;
+  final RegisterCubit registerCubit;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final TextEditingController repeatPasswordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          _chechFormKeyStateAndPasswordController(context);
+        },
+        child: Text(
+          LocaleKeys.buttons_register,
+          style: context.textTheme.headlineSmall,
+        ).tr(),
+      ),
+    );
+  }
+
+  void _chechFormKeyStateAndPasswordController(BuildContext context) {
+    if (formKey.currentState?.validate() ?? false) {
+      if (passwordController.text == repeatPasswordController.text) {
+        registerCubit.register(
+          email: emailController.text,
+          password: passwordController.text,
+        );
+      } else {
+        context.showErrorMessage('Password does not match');
+      }
+    }
+  }
+}
