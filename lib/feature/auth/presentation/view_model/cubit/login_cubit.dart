dccaf1c503657d9d4ac6807c78950e06086a760a
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-14 16:53:28 +0300

    Add LoginCubit for authentication feature

---
+++ lib/feature/auth/presentation/view_model/cubit/login_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc_example/feature/index.dart';
+
+/// LoginCubit
+final class LoginCubit extends Cubit<LoginState> {
+  /// LoginCubit constructor
+  LoginCubit(this._loginUseCase) : super(const LoginState.initial());
+  final LoginUseCase _loginUseCase;
+
+  /// signInWithEmailAndPassword
+  Future<void> signInWithEmailAndPassword(Users users) async {
+    emit(const LoginState.loading());
+    try {
+      await _loginUseCase.signInWithEmailAndPassword(users);
+      emit(const LoginState.success());
+    } catch (e) {
+      emit(LoginState.error(e.toString()));
+    }
+  }
+}
