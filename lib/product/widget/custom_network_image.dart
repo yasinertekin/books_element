5d8c9658ff75fbfa27aa06d3bdb499b81cfd4821
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-18 12:52:07 +0300

    Refactor CustomNetworkImage widget to support custom width and no image icon height

--- lib/product/widget/custom_network_image.dart
+++ lib/product/widget/custom_network_image.dart
@@ -2,33 +2,60 @@ import 'package:bloc_example/feature/index.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_svg/svg.dart';
 
+/// A widget that displays an image from the network.
 final class CustomNetworkImage extends StatelessWidget {
+  /// Creates a widget that displays an image from the network.
   const CustomNetworkImage({
     required this.imageUrl,
     this.fit = BoxFit.cover,
     this.height = 0.2,
     super.key,
+    this.width = 0.4,
+    this.borderRadius = 8,
+    this.icNoImageHeight = 0.08,
   });
 
+  /// The URL of the image to display.
   final String? imageUrl;
+
+  /// How the image should be inscribed into the box.
   final BoxFit fit;
+
+  /// The height of the image.
   final double height;
 
+  /// The width of the image.
+  final double width;
+
   String get _imgUrl => (imageUrl?.isEmpty ?? true) ? '' : imageUrl!;
+
+  /// The radius of the image.
+  final double borderRadius;
+
+  /// The height of the no image icon.
+  final double icNoImageHeight;
 
   @override
   Widget build(BuildContext context) {
     return _imgUrl.isEmpty
-        ? SvgPicture.asset(
-            Assets.icons.icNoImage,
-            height: context.dynamicHeight(0.13),
+        ? ClipRRect(
+            borderRadius: BorderRadius.circular(borderRadius),
+            child: SvgPicture.asset(
+              Assets.icons.icNoImage,
+              height: context.dynamicHeight(icNoImageHeight),
+            ),
           )
-        : CachedNetworkImage(
-            imageUrl: _imgUrl,
-            fit: fit,
-            height: context.dynamicHeight(height),
-            placeholder: (context, url) => const CircularProgressIndicator(),
-            errorWidget: (context, url, error) => const Icon(Icons.error),
+        : ClipRRect(
+            borderRadius: BorderRadius.circular(borderRadius),
+            child: CachedNetworkImage(
+              imageUrl: _imgUrl,
+              fit: fit,
+              width: context.dynamicWidth(width),
+              height: context.dynamicHeight(height),
+              placeholder: (context, url) =>
+                  const Center(child: CustomLoading()),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
           );
   }
 }
