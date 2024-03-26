cc0f4dd1a200660a3d898188c957aa6a026cbc35
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-26 19:58:13 +0300

    Add GoogleSignInCubit and update BlocProviders

--- lib/product/initialize/state_initialize.dart
+++ lib/product/initialize/state_initialize.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_example/feature/auth/presentation/view_model/cubit/google_sign_in_cubit.dart';
 import 'package:bloc_example/feature/index.dart';
 
 /// State Initialize
@@ -12,21 +13,22 @@ final class StateInitialize extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider<GoogleSignInCubit>(
+          create: (context) => Locator.googleSignInCubit,
+        ),
         BlocProvider<LoginCubit>(
           create: (context) => Locator.loginCubit,
         ),
-        BlocProvider(
+        BlocProvider<RegisterCubit>(
           create: (context) => Locator.registerCubit,
         ),
-        BlocProvider(
+        BlocProvider<TextFieldCubit>(
           create: (context) => Locator.textFieldCubit,
         ),
-        BlocProvider(
-          create: (context) => Locator.googleSignInCubit,
-        ),
-        BlocProvider(
+        BlocProvider<SearchBooksCubit>(
           create: (context) => Locator.booksCubit,
         ),
+        BlocProvider<HomeCubit>(create: (context) => Locator.homeCubit),
       ],
       child: child,
     );
