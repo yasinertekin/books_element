7ea28b551d9be47ecb205bd140030e743fd1bb6f
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-26 19:57:07 +0300

    Add books state class and remove unused code

--- lib/feature/search/presentation/view_model/state/books_state.dart
+++ lib/feature/search/presentation/view_model/state/books_state.dart
@@ -1,28 +1,33 @@
 import 'package:bloc_example/product/model/book/books/books.dart';
 import 'package:equatable/equatable.dart';
 
-final class BooksState extends Equatable {
-  const BooksState({
+final class SearchBooksState extends Equatable {
+  const SearchBooksState({
     this.books,
     this.error,
     this.isLoding = false,
+    this.flutter,
   });
 
   final Books? books;
   final String? error;
   final bool isLoding;
+  final Books? flutter;
 
-  BooksState copyWith({
+  SearchBooksState copyWith({
     Books? books,
     String? error,
     bool? isLoding,
+    Books? flutter,
+    z,
   }) =>
-      BooksState(
+      SearchBooksState(
         books: books ?? this.books,
         error: error ?? this.error,
         isLoding: isLoding ?? this.isLoding,
+        flutter: flutter ?? this.flutter,
       );
 
   @override
-  List<Object?> get props => [books, error, isLoding];
+  List<Object?> get props => [books, error, isLoding, flutter];
 }
---
+++ lib/feature/welcome/deneme.dart
@@ -0,0 +1,36 @@
+// final class Categories {
+//   const Categories({
+//     required this.name,
+//     required this.id,
+//     required this.icon,
+//     required this.dice,
+//   });
+//   final String name;
+//   final String id;
+//   final String icon;
+//   final List<Dice> dice;
+// }
+
+// final class Dice {
+//   const Dice({
+//     required this.name,
+//     required this.id,
+//     required this.icon,
+//     required this.diceElement,
+//   });
+//   final String name;
+//   final String id;
+//   final String icon;
+//   final List<DiceElement> diceElement;
+// }
+
+// final class DiceElement {
+//   const DiceElement({
+//     required this.name,
+//     required this.id,
+//     required this.image,
+//   });
+//   final String name;
+//   final String id;
+//   final String image;
+// }
