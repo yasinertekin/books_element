9a7d987811a190a84ba2545d99f4ed74a2951892
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-26 20:00:14 +0300

    Refactor sign-in methods widget and update imports

--- lib/product/widget/sign_in_methods.dart
+++ lib/product/widget/sign_in_methods.dart
@@ -1,10 +1,9 @@
-import 'package:bloc_example/feature/auth/view_model/cubit/google_sign_in_cubit.dart';
-import 'package:bloc_example/feature/auth/view_model/state/google_sign_in_state.dart';
+import 'package:bloc_example/feature/auth/presentation/view_model/cubit/google_sign_in_cubit.dart';
 import 'package:bloc_example/feature/index.dart';
+import 'package:bloc_example/product/core/extensions/show_error_extension.dart';
 
 /// Sign In Methods
-final class SignInMethods extends StatelessWidget
-    with ShowErrorMessageMixin, NavigationMixin {
+final class SignInMethods extends StatelessWidget {
   /// Sign In Methods Constructor
   const SignInMethods({super.key});
 
@@ -13,48 +12,58 @@ final class SignInMethods extends StatelessWidget
     return SizedBox(
       height: context.dynamicHeight(0.1),
       width: context.width,
-      child: BlocConsumer<GoogleSignInCubit, GoogleSignInState>(
-        listener: (context, state) {
-          state.maybeWhen(
-            orElse: () {},
-            error: (message) => showErrorMessage(context, message),
-            success: () => navigateToNamedRoute(
-              context,
-              NavigationEnum.navBarView.value,
-            ),
-          );
-        },
-        builder: (context, state) {
-          return state.maybeWhen(
-            orElse: () => Padding(
-              padding: context.paddingAllLow,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                    child: OutlinedButton(
-                      onPressed: () {
-                        context.read<GoogleSignInCubit>().googleSignIn(
-                              context,
-                            );
-                      },
-                      child: const Text('Google'),
-                    ),
-                  ),
-                  SizedBox(width: context.dynamicWidth(0.02)),
-                  Expanded(
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: const Text('Facebook'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            loading: () => const CustomLoading(),
-          );
-        },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const _GoogleSignInButton(),
+          SizedBox(width: context.dynamicWidth(0.02)),
+          const _FacebookButton(),
+        ],
       ),
+    );
+  }
+}
+
+final class _FacebookButton extends StatelessWidget {
+  const _FacebookButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () {},
+        child: const Text('Facebook'),
+      ),
+    );
+  }
+}
+
+final class _GoogleSignInButton extends StatelessWidget {
+  const _GoogleSignInButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<GoogleSignInCubit, GoogleSignInState>(
+      listener: (context, state) {
+        state.maybeWhen(
+          orElse: () {},
+          error: (message) => context.showErrorMessage(message),
+          success: () =>
+              context.navigateNamedTo(NavigationEnum.navBarView.value),
+        );
+      },
+      builder: (context, state) {
+        return Expanded(
+          child: OutlinedButton(
+            onPressed: () {
+              context.read<GoogleSignInCubit>().googleSignIn(
+                    context,
+                  );
+            },
+            child: const Text('Google'),
+          ),
+        );
+      },
     );
   }
 }
